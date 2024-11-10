6f7d24d3ffe9d229b0c1abde4767b4891ce81381
purush0t <purush0thaman@proton.me>
2024-11-10 15:04:03 +0530

    MCA plot code added

--- rna_seq_analysis.R
+++ rna_seq_analysis.R
@@ -6,7 +6,7 @@ BiocManager::install("dplyr")
 BiocManager::install("SummarizedEzperiment")
 BiocManager::install("DESeq2")
 BiocManager::install("pheatmap")
-BiocManager::install('EnhancedVolcano')
+BiocManager::install("EnhancedVolcano")
 BiocManager::install("clusterProfiler")
 BiocManager::install("enrichplot")
 
@@ -95,13 +95,61 @@ head(df_dseq)
 
 #creatin a copy of deseq , with new rownames (so the ensembl id dosent have version no.)
 ens_dseq <- filter_dseqobj
-rownames(ens_dseq_result) <- df_dseq$ENSEMBL
+rownames(ens_dseq) <- df_dseq$ENSEMBL
 
 
 #variance stabilization(vst)
 #For RNA-seq counts,the expected variance grows with the mean.so we do a variance stabilization transformation ,inside Dseq2 package
 normal_dseq <- vst(filter_dseqobj,blind = FALSE)
+#normal_dseq
 
-#PCA PLOT!
+
+#PCA(principle component analysis) PLOT!
+
+pca_results <-
+  plotPCA(
+    normal_dseq,
+    intgroup = c("condition"),
+    returnData = TRUE # This argument tells R to return the PCA values
+  )
+
+#plot using ggplot() function
+annotated_pca_plot<-ggplot(
+  pca_results,
+  aes(
+    x = PC1,
+    y = PC2,
+    # plot points with different colors for each `condition` group
+    shape=condition,
+    color= condition,
+    size = 1.5
+    # plot points with different shapes for each `condition` group
+  )
+) +
+  geom_point()
+annotated_pca_plot
+
+
+#MA plot
+MAplot_results <- plotMA(dseq_result,ylim=c(-2,2),main ="MA Plot",returnData = TRUE)
+
+#The plotMA() function returns a list containing two elements:
+# M: A vector of log2 fold change values.
+#A: A vector of average log2 expression levels.
+
+MAplot_results$lfc_category <- ifelse(MAplot_results$lfc < 0, "red",
+                              ifelse(MAplot_results$lfc == 0,"black", "purple"))
+
+colnames(MAplot_results)
+
+ggplot(MAplot_results,aes(x=mean,y=lfc))+
+  geom_point(color = MAplot_results$lfc_category) +
+  xlab("Mean Expression") +
+  ylab("Log2 Fold Change") +
+  ggtitle("MA Plot with Colored Points based on lfc")+
+  scale_x_continuous(limits = c(0, 1000), breaks = seq(0, 40000, by = 40000)) +
+  scale_y_continuous(limits = c(-3, 3), breaks = seq(-3, 3, by = 1))
+
+
 
 
