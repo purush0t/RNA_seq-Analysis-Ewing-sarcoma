376df934cf293390aef8422ad89d255fd4472429
purush0t <purush0thaman@proton.me>
2024-11-10 15:00:43 +0530

    rna seq analysis r script

---
+++ rna_seq_analysis.R
@@ -0,0 +1,107 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install('org.Hs.eg.db')
+BiocManager::install('AnnotationDbi')
+BiocManager::install("dplyr")
+BiocManager::install("SummarizedEzperiment")
+BiocManager::install("DESeq2")
+BiocManager::install("pheatmap")
+BiocManager::install('EnhancedVolcano')
+BiocManager::install("clusterProfiler")
+BiocManager::install("enrichplot")
+
+library(dplyr)
+library(SummarizedExperiment)
+library(DESeq2)
+library(pheatmap)
+library(EnhancedVolcano)
+library(org.Hs.eg.db)
+library(kableExtra)
+library(ggplot2)
+library(clusterProfiler)
+library(enrichplot)
+library(DOSE)
+library(ggridges)
+library(tidyverse)
+library(pathview)
+library(DESeq2)
+
+# data is of object type:rangedsummarizedexperiment data 
+rse <- readRDS("EwS.rds")
+rse
+
+#assay function extracts the primary assay data from rds
+primary_countsdata <- assay(rse)
+primary_countsdata
+dim_primary_countsdata <- dim(primary_countsdata)
+#print(paste("dimension of p_countsdata:",dim_primary_countsdata))
+dim_primary_countsdata
+
+#colData(rse)
+# colData accessing all the meta data
+meta_data <- data.frame(colData(rse))
+
+#getiin all the column names from meta data
+
+SRA_accesion <- rownames(meta_data)
+class(SRA_accesion)
+
+
+#condition_data <- data.frame("SRA"=SRA_accesion,"condition"=meta_data$condition)
+condition_data <- data.frame(condition = meta_data$condition,
+                             row.names = meta_data$run)
+condition_data
+##type(condition_data)
+
+#removing entries where the read counts are 0
+filtered_counts_data <- primary_countsdata[which(rowSums(primary_countsdata)>0),]
+head(filtered_counts_data )
+
+dim(filtered_counts_data)
+
+#check if all the sam
+all(rownames(condition_data) %in% colnames(filtered_counts_data))
+
+
+dseq_object <- DESeqDataSetFromMatrix(countData = filtered_counts_data,colData = condition_data, design = ~condition)
+
+dseq_object
+#removing entries with low read counts
+dseq_temp <- rowSums(counts(dseq_object)) >=100
+
+filter_dseqobj <- dseq_object[dseq_temp]
+
+# the control sample "shCTR is used as reference for Dseq"
+filter_dseqobj$Treatment <- relevel(filter_dseqobj$condition,ref = "shCTR")
+filter_dseqobj$Treatment
+
+#running the Dseq2
+
+filter_dseqobj <- DESeq(filter_dseqobj)
+
+dseq_result <- results(filter_dseqobj)
+dseq_result
+summary(dseq_result)
+
+# using pipes to , add new cloumns, remove version no.from ensembl id's , mapping to respective gene name's
+df_dseq <- data.frame(dseq_result) %>%
+  add_column(gene_id = 0, version = rownames(.))%>%
+  mutate(ENSEMBL=gsub("\\..*","",rownames(.)))%>%
+  mutate(gene_id = mapIds(org.Hs.eg.db,keys = .$ENSEMBL,keytype = "ENSEMBL",column = "SYMBOL"))%>%
+  `rownames<-`(.$ENSEMBL)
+#colnames(df_dseq)
+#rownames(df_dseq)
+head(df_dseq)
+
+#creatin a copy of deseq , with new rownames (so the ensembl id dosent have version no.)
+ens_dseq <- filter_dseqobj
+rownames(ens_dseq_result) <- df_dseq$ENSEMBL
+
+
+#variance stabilization(vst)
+#For RNA-seq counts,the expected variance grows with the mean.so we do a variance stabilization transformation ,inside Dseq2 package
+normal_dseq <- vst(filter_dseqobj,blind = FALSE)
+
+#PCA PLOT!
+
+
